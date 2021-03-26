75ddb6e88018af915872dcda30ab1d4ebc31d380
Diego Rios Vasquez <diegovasquezcolombia@gmail.com>
2021-03-26 17:06:04 -0500

    ProyectoPronosticoStock

---
+++ ProyectoPronosticoStock.sql
@@ -0,0 +1,10 @@
+------------------- Proyecto Pronostico Stock -------------------
+
+CREATE PROC PronosticoStock
+@FechaInicial DATE,
+@FechaFinal DATE,
+@Referencia NVARCHAR(50),
+@CodigoBodega VARCHAR(50)
+AS
+    
+GO
