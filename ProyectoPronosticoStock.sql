151e2ea12313d84e97920222863852641f7d2ccb
Diego Rios Vasquez <diegovasquezcolombia@gmail.com>
2021-03-29 12:50:38 -0500

    Reorden de Campos Stored Procedure

--- ProyectoPronosticoStock.sql
+++ ProyectoPronosticoStock.sql
@@ -6,8 +6,7 @@ CREATE PROC PronosticoStock
 @Referencia NVARCHAR(50),
 @CodigoBodega VARCHAR(50)
 AS
-  DECLARE @Stock FLOAT
-  SELECT PB.Stock -ADU + CantTotalInsumo AS Stock, PB.Stock AS Stock_Inicial, PB.ADU, Bodega.Codigo, PB.Referencia, FechaEntrega CantTotalInsumo FROM Vs_InventarioEnTransitoAcumulado_BETA, Tbl_ProductoBodega PB
+  SELECT FechaEntrega AS Dia, PB.Stock - ADU + CantTotalInsumo AS Stock, PB.Stock AS Stock_Inicial, PB.ADU, Bodega.Codigo, PB.Referencia, Producto.Descripcion, CantTotalInsumo FROM Vs_InventarioEnTransitoAcumulado_BETA, Tbl_ProductoBodega PB
   INNER JOIN Tbl_Bodegas Bodega on PB.IdBodega = (SELECT IdBodega FROM Tbl_Bodegas WHERE Codigo = @CodigoBodega)
   INNER JOIN Tbl_Productos Producto ON PB.IdProducto = (SELECT IdProducto FROM Tbl_Productos WHERE Referencia = @Referencia) WHERE (FechaEntrega BETWEEN @FechaInicial AND @FechaFinal)
 GO
