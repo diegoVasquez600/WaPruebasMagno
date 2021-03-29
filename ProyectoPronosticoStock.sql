ae0c5a2e839a51269157e4686a588c467aff2def
Diego Rios Vasquez <diegovasquezcolombia@gmail.com>
2021-03-29 12:35:42 -0500

    ProyectoPronostico Store Procedure

--- ProyectoPronosticoStock.sql
+++ ProyectoPronosticoStock.sql
@@ -6,5 +6,8 @@ CREATE PROC PronosticoStock
 @Referencia NVARCHAR(50),
 @CodigoBodega VARCHAR(50)
 AS
-    
+  DECLARE @Stock FLOAT
+  SELECT PB.Stock -ADU + CantTotalInsumo AS Stock, PB.Stock AS Stock_Inicial, PB.ADU, Bodega.Codigo, PB.Referencia, FechaEntrega CantTotalInsumo FROM Vs_InventarioEnTransitoAcumulado_BETA, Tbl_ProductoBodega PB
+  INNER JOIN Tbl_Bodegas Bodega on PB.IdBodega = (SELECT IdBodega FROM Tbl_Bodegas WHERE Codigo = @CodigoBodega)
+  INNER JOIN Tbl_Productos Producto ON PB.IdProducto = (SELECT IdProducto FROM Tbl_Productos WHERE Referencia = @Referencia) WHERE (FechaEntrega BETWEEN @FechaInicial AND @FechaFinal)
 GO
